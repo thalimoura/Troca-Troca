0222ffe07b61fac4790a600ea4a6bca929667412
thalimoura <165019843+thalimoura@users.noreply.github.com>
2024-03-26 09:08:40 -0300

    Add files via upload

---
+++ Troca Troca.py
@@ -0,0 +1,8 @@
+def Troca_Troca():
+    valor_A =  int(input("informe o valor de A:"))
+    valor_B =  int(input("informe o valor de B:"))
+
+    print(f"valor de A {valor_B}")
+    print(f"valor de B {valor_A}")
+
+Troca_Troca()
